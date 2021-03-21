a3621ac33a55b50f7c9ffb9a611dc3161e081cb5
xiaofeng sun <xfsun_dlmu@163.com>
2021-03-21 09:15:08 +0800

    增加add函数

--- foo.cpp
+++ foo.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+
+int add(int x, y)
+{
+	return x + y;
+}
 int main(int argc, char const *argv[])
 {
 	/* code */
 	std::cout << "Hello world!" << std::endl;
+
+	int num = add(1, 2);
+	std::cout << num << std::endl;
+
 	return 0;
 }
